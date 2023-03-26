19954015f3bfd278727f2240d7fe6a33eac217fd
hakimled <hakimled30@gmail.com>
2023-03-27 00:22:53 +0100

    modified JavaScript file facebook.js

--- facebook.js
+++ facebook.js
@@ -2,5 +2,5 @@
 const bodyEl = document.querySelector('body');
 
 document.querySelector('body').addEventListener('click', function(){
-    bodyEl.style.backgroundColor = 'lightgreen';
+    bodyEl.style.backgroundColor = 'lightblue';
 });
